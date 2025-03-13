b97edc6b3ce803d1421fa40cbd5f5fc80a794b47
WmDo <wdo008@ucr.edu>
2025-03-12 18:13:27 -0700

    Implement error handling logic

---
+++ backend/src/middleware/errorHandling/authError.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+
+const authErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    if (err.status === 401 || err.name === "UnauthorizedError") {
+        return res.status(401).json({ message: "Invalid credentials."});
+    }
+
+    if (err.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "Session expired. Please log in again." });
+    }
+
+    if (err.name === "JsonWebTokenError") {
+        return res.status(401).json({ message: "Authentication failed due to invalid token." });
+    }
+
+    if (err.code === 11000) {   // MongoDB duplicate key error
+        return res.status(400).json({ message: "Username or email already exists." });
+    }
+
+    if (err.status === 400) {   
+        return res.status(400).json({ message: err.message || "Invalid request." });
+    }
+    
+    next(err);
+};
+
+export default authErrorHandler;
