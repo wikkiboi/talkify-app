ac0ea44053543bb061af181ded4a58a447aa6fda
wikkiboi <mofouyu@gmail.com>
2025-03-13 02:31:21 -0700

    Wording change to 'Join with Invite Code' in space option modal

--- frontend/src/components/modals/NewServerOptionsModal.tsx
+++ frontend/src/components/modals/NewServerOptionsModal.tsx
@@ -26,9 +26,11 @@ export default function NewServerOptionsModal({
           <div className="modal-content">
             <h3>Choose an option</h3>
             <button onClick={handleClick} value="create">
-              Create a Server
+              Create a Space
             </button>
-            <button onClick={() => setModalType("join")}>Join with ID</button>
+            <button onClick={() => setModalType("join")}>
+              Join with Invite Code
+            </button>
             <button onClick={() => setModalType("logout")}>Logout</button>
             <button onClick={() => showModal(false)}>Cancel</button>
           </div>
