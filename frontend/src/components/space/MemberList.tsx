7b933e40269b22ca7c1a64573af6d8735a35231f
Bao Huynh <baohuynh888@gmail.com>
2025-03-13 17:09:23 -0700

    Fixed members list sidebar and style

--- frontend/src/components/space/MemberList.tsx
+++ frontend/src/components/space/MemberList.tsx
@@ -5,17 +5,20 @@ interface UserListProps {
 }
 
 export default function MemberList({ users }: UserListProps) {
-  const userList = users.map((user) => {
-    return (
-      <li key={user.userId}>
-        {user.username}: {user.status}
-      </li>
-    );
-  });
   return (
-    <div>
-      Members List
-      <ul>{userList}</ul>
+    <div className="members-sidebar">
+      <h3 className="members-header">Space Members</h3>
+      <div className="members-list">
+        {users.length > 0 ? (
+          users.map((user) => (
+            <button key={user.userId} className="member-item">
+              {user.username}: <span>{user.status}</span>
+            </button>
+          ))
+        ) : (
+          <p>No members in this space</p>
+        )}
+      </div>
     </div>
   );
 }
